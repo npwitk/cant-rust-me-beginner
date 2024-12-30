4a82e2371473c10e358dc117bc52207dfa38ea6f
Nonprawich I. <40120130+npwitk@users.noreply.github.com>
2024-12-30 14:41:57 +0700

    Add hello, world

---
+++ A-hello-crabby/target/debug/deps/A_hello_crabby-6419b19159d4eb31.d
@@ -0,0 +1,5 @@
+/Users/npwitk/Library/Mobile Documents/com~apple~CloudDocs/Courses/Rust Mastery Saga/cant-rust-me-beginner/A-hello-crabby/target/debug/deps/libA_hello_crabby-6419b19159d4eb31.rmeta: src/main.rs
+
+/Users/npwitk/Library/Mobile Documents/com~apple~CloudDocs/Courses/Rust Mastery Saga/cant-rust-me-beginner/A-hello-crabby/target/debug/deps/A_hello_crabby-6419b19159d4eb31.d: src/main.rs
+
+src/main.rs:
---
+++ A-hello-crabby/target/debug/deps/A_hello_crabby-900b4d5e60b8f801.d
@@ -0,0 +1,5 @@
+/Users/npwitk/Library/Mobile Documents/com~apple~CloudDocs/Courses/Rust Mastery Saga/cant-rust-me-beginner/A-hello-crabby/target/debug/deps/libA_hello_crabby-900b4d5e60b8f801.rmeta: src/main.rs
+
+/Users/npwitk/Library/Mobile Documents/com~apple~CloudDocs/Courses/Rust Mastery Saga/cant-rust-me-beginner/A-hello-crabby/target/debug/deps/A_hello_crabby-900b4d5e60b8f801.d: src/main.rs
+
+src/main.rs:
